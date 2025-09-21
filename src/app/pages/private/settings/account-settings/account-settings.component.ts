1b4dfaa18d03a70b68abea0809ef61210f4af4b1
RubenFern <rubenferval@hotmail.com>
2025-09-21 17:04:29 +0200

    delete account button added

--- src/app/pages/private/settings/account-settings/account-settings.component.ts
+++ src/app/pages/private/settings/account-settings/account-settings.component.ts
@@ -1,15 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MaterialModules } from '@material/material.modules';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 import { EditPasswordComponent } from './edit-password/edit-password.component';
 import { EditUsernameComponent } from './edit-username/edit-username.component';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogConfirmComponent } from '../../../../components/dialog-confirm/dialog-confirm.component';
 
 @Component({
   selector: 'app-account-settings',
   imports: [
     EditPasswordComponent,
     EditUsernameComponent,
-    MaterialModules
+    MaterialModules,
+    TranslatePipe
   ],
   templateUrl: './account-settings.component.html',
   styles: ``,
@@ -17,7 +21,23 @@ import { EditUsernameComponent } from './edit-username/edit-username.component';
 })
 export class AccountSettingsComponent {
 
+  constructor(
+    private dialog: MatDialog,
+        private translateService: TranslateService
+  ) { }
 
+  deleteAccount() {
+    const dialogRef = this.dialog.open(DialogConfirmComponent, {
+      data: {
+        title: this.translateService.instant('settings.account.delete-account-confirmation'),
+        message: this.translateService.instant('settings.account.delete-account-message'),
+        focusCancel: true
+      }
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+
+    });
+  }
 
 }
